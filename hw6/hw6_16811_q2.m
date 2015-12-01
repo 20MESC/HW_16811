5b76f2167e6fecfa6bdd10132ab2ef5d953080f5
Rosario Scalise <romesc@live.com>
2015-12-01 06:21:42 -0500

    Progress on visibility graph.

--- hw6/hw6_16811_q2.m
+++ hw6/hw6_16811_q2.m
@@ -4,14 +4,20 @@
 poly_struct = struct('v',[]);
 %poly_struct(1).v = [2 2 7 7; 1 7 7 1];
 poly_struct(1).v = [2 0 4 10 7; 2 6 10 7 3];
+poly_struct(2).v = [7 7 9 9; -2 0 0 -2];
 
 plot(poly_struct(1).v(1,:),poly_struct(1).v(2,:),'ro')
-axis([-2 15 -2 15])
+axis([-5 15 -5 15])
 hold on;
 
 % Plot the edges of an arbitrary polygon
-nV = size(poly_struct(1).v,2);
-plot(poly_struct(1).v(1,:),poly_struct(1).v(2,:),'g-');
-plot([poly_struct(1).v(1,nV) poly_struct(1).v(1,1)],[poly_struct(1).v(2,nV) poly_struct(1).v(2,1)],'g-')
+for i = 1:size(poly_struct,2)
+    % plot vertices
+    plot(poly_struct(i).v(1,:),poly_struct(i).v(2,:),'ro')
+    nV = size(poly_struct(i).v,2);
+    % plot edges
+    plot(poly_struct(i).v(1,:),poly_struct(i).v(2,:),'g-');
+    plot([poly_struct(i).v(1,nV) poly_struct(i).v(1,1)],[poly_struct(i).v(2,nV) poly_struct(i).v(2,1)],'g-')
+end
 
 [nodes] = visibilityGraph(poly_struct)
